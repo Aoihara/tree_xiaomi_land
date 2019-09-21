3411c9212875e8c8b4cece09dc1de155e68f0d0f
bowgotsai <bowgotsai>
2019-09-21 03:42:57 +0000

    land: Adding GSI keys

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 
 LOCAL_PATH := device/xiaomi/land
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
