252331dc8e9f6ba21a6c2000bb12ac39af7192a5
Aoihara <disaachan@gmail.com>
2020-02-29 07:28:58 +0100

    land: move to ProductConfigQcom

--- device.mk
+++ device.mk
@@ -380,6 +380,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# QCom Support
+PRODUCT_BOARD_PLATFORM := msm8937
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
