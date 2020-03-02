207ea1eebfe81ccd2d2f620fa79c43077c907b88
ghostrider-reborn <radithya2002@gmail.com>
2020-03-02 00:40:27 +0000

    land: Enable low ram flag for 2GB variants
    - With pinner service and other stuff, free RAM ends up less than 900MB

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -85,6 +85,10 @@ void check_device()
         heapsize = "512m";
         heapminfree = "2m";
         heapmaxfree = "8m";
+
+        // Enable low ram flag
+        property_set("ro.config.low_ram", "true");
+
    }
 }
 
